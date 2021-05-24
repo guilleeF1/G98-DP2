36a6b15f538b2a245c8f7f77856b473db6955bd4
Fran <fjbelrab@gmail.com>
2021-05-24 02:07:52 +0200

    Solucionado problema de comentarios con los test

--- src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListTest.java
+++ src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListTest.java
@@ -18,6 +18,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebElement;
@@ -32,9 +33,9 @@ public class AuthenticatedTaskListTest extends AcmePlannerTest {
 	// Test cases -------------------------------------------------------------
 	
 	// Prueba de comprobación de que se pueda obtener la lista de todas las tasks finalizadas correctamente entrando en la api logueándose como cualquier usuario
-//	@ParameterizedTest
-//	@CsvFileSource(resources = "/authenticated/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
-//	@Order(10)	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/task/list.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(10)	
 	public void list(final int recordIndex, final String publica, final String titulo, final String periodoEjecucionInicio,
 		final String periodoEjecucionFinal, final String cargaTrabajo, final String cargaTrabajoMinutos, 
 		final String descripcion, final String enlace) {		
@@ -68,8 +69,8 @@ public class AuthenticatedTaskListTest extends AcmePlannerTest {
 	//---------------------------------------------------------------------------------------------------------
 	
 	//Este test comprueba que un usuario con rol de administrador no puede acceder a la lista de tareas que tienen los usuarios registrados (authenticated)
-//	@Test
-//	@Order(10)
+	@Test
+	@Order(10)
 	public void listAuthenticatedTaskNegative() {		
 		super.signIn("administrator", "administrator");	
 		
@@ -82,8 +83,8 @@ public class AuthenticatedTaskListTest extends AcmePlannerTest {
 	//Como resultado, se generará un error de tipo Panic, ya que estamos accediendo a una funcionalidad para la que no tenemos los permisos adecuados
 	
 	//Este test comprueba que los usuarios sin registrar no pueden acceder a la lista de tareas que tienen los usuarios registrados(authenticated)
-//		@Test
-//		@Order(10)
+		@Test
+		@Order(10)
 		public void listAuthenticatedTaskNegative2() {		
 			
 			super.navigate("/authenticated/task/list-finished","");
